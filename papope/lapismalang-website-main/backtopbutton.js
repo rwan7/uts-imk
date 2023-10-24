dd962b231b770dc12586b4b32fb89edb90dd5d48
rwan7 <135501964+rwan7@users.noreply.github.com>
2023-10-24 11:50:52 +0700

    Add files via upload

---
+++ papope/lapismalang-website-main/backtopbutton.js
@@ -0,0 +1,7 @@
+let backbutton = document.getElementById("bckbtn");
+
+function topFunction() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+
+  
---
+++ papope/lapismalang-website-main/blog/nav-hp.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector('.navigate-resp-img')
+const menu = document.querySelector('.navigate-resp-div')
+
+toggle.onclick = function(){
+    menu.classList.toggle('open')
+}
